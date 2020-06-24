18ec8dd62b02927c45b6e324a23ecf532e071d12
Crowdbotics <team@crowdbotics.com>
2020-06-24 21:18:53 +0000

    Update components

--- src/features/BlankScreen871064/screens/blank.js
+++ src/features/BlankScreen871064/screens/blank.js
@@ -42,7 +42,7 @@ export class _Blank extends React.Component {
           textAlign: "left",
           verticalAlign: "baseline",
           fontSize: "20pt",
-          flexDirection: "column",
+          flexDirection: "row",
           flexWrap: "wrap",
           justifyContent: "flex-start",
           alignItems: "stretch",
@@ -63,161 +63,27 @@ export class _Blank extends React.Component {
             alignItems: "stretch",
             alignContent: "stretch",
             borderStyle: "solid",
-            backgroundSize: "auto"
+            backgroundSize: "auto",
+            width: "70%"
           }}
         >
-          <View
+          <Text
             style={{
               overflow: "visible",
               textAlign: "left",
               verticalAlign: "baseline",
               fontSize: "20pt",
-              flexDirection: "row",
-              flexWrap: "wrap",
-              justifyContent: "flex-start",
-              alignItems: "stretch",
-              alignContent: "stretch",
               borderStyle: "solid",
-              backgroundSize: "auto"
+              fontWeight: "bold"
             }}
           >
-            <View
-              style={{
-                overflow: "visible",
-                textAlign: "left",
-                verticalAlign: "baseline",
-                fontSize: "20pt",
-                flexDirection: "column",
-                flexWrap: "wrap",
-                justifyContent: "center",
-                alignItems: "stretch",
-                alignContent: "flex-start",
-                borderStyle: "solid",
-                backgroundSize: "auto",
-                width: "2%"
-              }}
-            >
-              <Text
-                style={{
-                  overflow: "visible",
-                  textAlign: "left",
-                  verticalAlign: "baseline",
-                  fontSize: "20pt",
-                  borderStyle: "solid"
-                }}
-              >
-                12:00pm
-              </Text>
-            </View>
-            <View
-              style={{
-                overflow: "visible",
-                textAlign: "left",
-                verticalAlign: "baseline",
-                fontSize: "20pt",
-                flexDirection: "column",
-                flexWrap: "wrap",
-                justifyContent: "flex-start",
-                alignItems: "stretch",
-                alignContent: "stretch",
-                borderStyle: "solid",
-                backgroundSize: "auto"
-              }}
-            >
-              <View
-                style={{
-                  overflow: "visible",
-                  textAlign: "left",
-                  verticalAlign: "baseline",
-                  fontSize: "20pt",
-                  flexDirection: "column",
-                  flexWrap: "wrap",
-                  justifyContent: "flex-start",
-                  alignItems: "stretch",
-                  alignContent: "stretch",
-                  borderStyle: "solid",
-                  backgroundSize: "auto"
-                }}
-              >
-                <View
-                  style={{
-                    overflow: "visible",
-                    textAlign: "left",
-                    verticalAlign: "baseline",
-                    fontSize: "20pt",
-                    flexDirection: "column",
-                    flexWrap: "wrap",
-                    justifyContent: "flex-end",
-                    alignItems: "stretch",
-                    alignContent: "stretch",
-                    borderStyle: "solid",
-                    backgroundSize: "auto"
-                  }}
-                >
-                  <Text
-                    style={{
-                      overflow: "visible",
-                      textAlign: "left",
-                      verticalAlign: "baseline",
-                      fontSize: "10pt",
-                      borderStyle: "solid"
-                    }}
-                  >
-                    Grand Ballroom
-                  </Text>
-                </View>
-                <View
-                  style={{
-                    overflow: "visible",
-                    textAlign: "left",
-                    verticalAlign: "baseline",
-                    fontSize: "20pt",
-                    flexDirection: "column",
-                    flexWrap: "wrap",
-                    justifyContent: "flex-start",
-                    alignItems: "stretch",
-                    alignContent: "stretch",
-                    borderStyle: "solid",
-                    backgroundSize: "auto"
-                  }}
-                >
-                  <Text
-                    style={{
-                      overflow: "visible",
-                      textAlign: "left",
-                      verticalAlign: "baseline",
-                      fontSize: "14pt",
-                      fontWeight: "bold",
-                      borderStyle: "solid"
-                    }}
-                  >
-                    Event Title
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
+            Event Title
+          </Text>
         </View>
         <View
           style={{
             overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto",
-            height: "200pt"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
+            textAlign: "right",
             verticalAlign: "baseline",
             fontSize: "20pt",
             flexDirection: "column",
@@ -228,98 +94,60 @@ export class _Blank extends React.Component {
             borderStyle: "solid",
             backgroundSize: "auto"
           }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
-        <View
-          style={{
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: "20pt",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderStyle: "solid",
-            backgroundSize: "auto"
-          }}
-        />
+        >
+          <Text
+            style={{
+              overflow: "visible",
+              textAlign: "right",
+              verticalAlign: "baseline",
+              fontSize: "14pt",
+              borderStyle: "solid"
+            }}
+          >
+            12:00pm
+          </Text>
+        </View>
       </View>
+      <Text
+        style={{
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: "12pt",
+          borderStyle: "solid",
+          fontWeight: "bold"
+        }}
+      >
+        Grand Ballroom
+      </Text>
+      <Text
+        style={{
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: "12pt",
+          borderStyle: "solid",
+          fontStyle: "italic"
+        }}
+      >
+        Floor 3, Room 412
+      </Text>
+      <Text
+        style={{
+          paddingBottom: "450pt",
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: "10pt",
+          borderStyle: "solid",
+          paddingTop: "10pt",
+          paddingLeft: "10pt",
+          paddingRight: "10pt"
+        }}
+      >
+        A description of the event. A description of the event. A description of
+        the event. A description of the event.
+      </Text>
     </View>
   )
 }
